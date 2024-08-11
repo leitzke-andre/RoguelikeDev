ce55607d7fe194578af3ff9b3f7312ee4666af28
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2024-08-11 19:06:16 +0200

    Add initial game map system

--- actions.py
+++ actions.py
@@ -1,11 +1,43 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity):
+        """Perform this action with the objects needed to determine its scope
+
+        `engine` is the scope this action is being performed in.
+
+        `entity` is the entity performing the action.
+
+        This method must be overridden by the subclasses.
+        """
+        return NotImplementedError()
+
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity):
+        raise SystemExit()
+
 
 class MovementAction(Action):
+
     def __init__(self, dx, dy):
         super().__init__()
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity):
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is outside map boundaries
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination tile is not walkable // Path is blocked
+
+        entity.move(self.dx, self.dy)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import tcod
 
-from typing import Tuple
 from entity import Entity
-from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler
 from engine import Engine
+from game_map import GameMap
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -21,7 +23,9 @@ def main() -> None:
     npc = Entity((screen_width // 2) - 5, screen_height // 2, "@", (255, 255, 0))
     entities = {player, npc}
 
-    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -36,6 +40,7 @@ def main() -> None:
             engine.render(console=root_console, context=context)
             events = tcod.event.wait()
             engine.handle_events(events)
+
 
 if __name__ == '__main__':
     main()
